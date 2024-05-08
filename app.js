936ef2d862d88bf14f9cca379db59da81d4d0730
gogiabu <146556307+gogiabu@users.noreply.github.com>
2024-05-08 13:42:56 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+var m =  [12, 67, 34, 122, 10, -90, -12, -100, -546, 3452, -1, -2];
+
+for (let i = 0; i < m.length; i++) {
+    if( m[i] < 0) {
+        document.write (m[i] + "<br><br>");
+    }
+}
+for (let g = 0; g < m.length; g++) {
+    if( m[g] > 0) {
+        document.write (m[g] + "<br>");
+    }
+}
+ 
